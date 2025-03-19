6285a9da1b70b2cb03ca99b1a09752b6d9deecbd
raphdf201 <raphdf201@hotmail.com>
2025-03-18 21:21:53 -0400

    remove out folder and add test folder

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from scss compiler!');
-		console.log(compileSCSS("scssCompileVScode/test.scss"));
+		console.log(compileSCSS("test.scss"));
 	});
 
 	context.subscriptions.push(disposable);
